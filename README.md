c2e990aa59adaae7c1ecb480ac0e48d09bf5acc4
Stephan Verbücheln <verbuecheln@posteo.de>
2020-12-28 13:44:45 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,29 @@
+# rC3 Stream Starter
+
+Starts rC3 streams with the celluloid (former gnome-mpv) or any other favorite
+video player.
+
+## Why?
+
+Watching streams in the browser slows down the computer, draining battery and
+consuming resources that could be used for the rc3 World.
+
+## How?
+
+### Requirements
+```
+$ sudo apt install gnome-devel celluloid
+```
+
+### Installation
+```
+$ git clone https://github.com/erlenmayr/streamlauncher
+$ cd streamlauncher
+$ meson build
+$ ninja -C build
+$ ninja -C build install
+```
+
+### Hacking
+
+I recommend Gnome Builder (should be installed by gnome-devel) to easily hack
---
+++ src/window.py
@@ -0,0 +1,37 @@
+# window.py
+#
+# Copyright 2020 Stephan Verbücheln
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from gi.repository import Gtk
+import subprocess
+
+
+
+@Gtk.Template(resource_path='/de/ccc/streamlauncher/window.ui')
+class StreamlauncherWindow(Gtk.ApplicationWindow):
+    __gtype_name__ = 'StreamlauncherWindow'
+
+    en_player = Gtk.Template.Child()
+
+    @Gtk.Template.Callback('on_button_clicked')
+    def _on_button_clicked(self, button):
+        stream = button.get_label()
+        url = 'https://cdn.c3voc.de/' + stream
+        subprocess.Popen([self.en_player.get_text(), url])
+
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
